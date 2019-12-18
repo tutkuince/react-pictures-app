cceaba22c3a761ac5e81fb2b15a01dafc7f27231
tutkuince <tutku.ince@outlook.com>
2019-12-18 22:58:34 +0300

    SearchBar component is created and used in App.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <input type="text" name="name" id=""/>
+                </form>                
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
