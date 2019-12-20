56133ab433058b784922d5283155aee1a5eee1b9
tutkuince <tutku.ince@outlook.com>
2019-12-20 14:53:12 +0300

    solving context issue with arrow function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,22 +11,22 @@ class SearchBar extends React.Component {
 
     state = {term: ''};
 
-    onFormSubmit(event) {
-        // this is prevent automatically refreshing the page.
-        event.preventDefault();
-
-        // error.
-        console.log(this.state.term);
-    }
-
-    // second way
-    // onFormSubmit = (event) => {
+    // onFormSubmit(event) {
     //     // this is prevent automatically refreshing the page.
     //     event.preventDefault();
     //
-    //     // arrow function automatically bind this function
+    //     // error.
     //     console.log(this.state.term);
     // }
+
+    // second way
+    onFormSubmit = (event) => {
+        // this is prevent automatically refreshing the page.
+        event.preventDefault();
+
+        // arrow function automatically bind this function
+        console.log(this.state.term);
+    }
 
     // this.onInputChange() -> this method will be called when component is rendered.
     // if we don't want to do this, we should use it without parentheses
@@ -34,7 +34,8 @@ class SearchBar extends React.Component {
         return (
             <div className="ui segment">
                 {/*// this is the third way for binding*/}
-                <form onSubmit={event => {this.onFormSubmit(event)}} action="" className="ui form">
+                {/*<form onSubmit={event => {this.onFormSubmit(event)}} action="" className="ui form">*/}
+                <form onSubmit={this.onFormSubmit} action="" className="ui form">
                     <div className="field">
                         <label htmlFor="search">Image Search</label>
                         <input type="text"
