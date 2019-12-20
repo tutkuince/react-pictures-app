bd72a39a0d162c50d70a3e7961091fd176e1dfe1
tutkuince <tutku.ince@outlook.com>
2019-12-20 14:48:57 +0300

    binding

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,21 @@ class SearchBar extends React.Component {
 
     state = {term: ''};
 
-    onFormSubmit(event) {
+    // onFormSubmit(event) {
+    //     // this is prevent automatically refreshing the page.
+    //     event.preventDefault();
+    //
+    //     // error.
+    //     console.log(this.state.term);
+    // }
+
+    // second way
+    onFormSubmit = (event) => {
         // this is prevent automatically refreshing the page.
         event.preventDefault();
+
+        // arrow function automatically bind this function
+        console.log(this.state.term);
     }
 
     // this.onInputChange() -> this method will be called when component is rendered.
