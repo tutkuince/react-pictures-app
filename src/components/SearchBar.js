aab6aab3e9e7298b2bda0ad9459dd8dd7d481b90
tutkuince <tutku.ince@outlook.com>
2019-12-20 14:50:28 +0300

    binding 3th way.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,29 +4,30 @@ class SearchBar extends React.Component {
 
     state = {term: ''};
 
-    // onFormSubmit(event) {
-    //     // this is prevent automatically refreshing the page.
-    //     event.preventDefault();
-    //
-    //     // error.
-    //     console.log(this.state.term);
-    // }
-
-    // second way
-    onFormSubmit = (event) => {
+    onFormSubmit(event) {
         // this is prevent automatically refreshing the page.
         event.preventDefault();
 
-        // arrow function automatically bind this function
+        // error.
         console.log(this.state.term);
     }
+
+    // second way
+    // onFormSubmit = (event) => {
+    //     // this is prevent automatically refreshing the page.
+    //     event.preventDefault();
+    //
+    //     // arrow function automatically bind this function
+    //     console.log(this.state.term);
+    // }
 
     // this.onInputChange() -> this method will be called when component is rendered.
     // if we don't want to do this, we should use it without parentheses
     render() {
         return (
             <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} action="" className="ui form">
+                {/*// this is the third way for binding*/}
+                <form onSubmit={event => {this.onFormSubmit(event)}} action="" className="ui form">
                     <div className="field">
                         <label htmlFor="search">Image Search</label>
                         <input type="text"
