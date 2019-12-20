320da1e5cfed0a252b4c7afccaf46a7701784925
tutkuince <tutku.ince@outlook.com>
2019-12-20 19:21:10 +0300

    viewing axios get request

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import axios from 'axios';
+
 import SearchBar from "./SearchBar";
+
 
 class App extends React.Component {
 
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID 177b21be1c6a9b01f891a40839c277ebb078e69f54e983039da392a8757969d1'
+            }
+        });
     }
 
     render() {
