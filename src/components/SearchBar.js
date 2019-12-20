e9d144b7baf5cc0512bc24439b4506a3eb4a5fe2
tutkuince <tutku.ince@outlook.com>
2019-12-20 13:36:39 +0300

    onInputChange with event object

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,23 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
-    onInputChange() {
-
+    onInputChange(event) {
+        console.log(event.target.value);
     }
 
-
+    // this.onInputChange() -> this method will be called when component is rendered.
+    // if we don't want to do this, we should use it without parentheses
     render() {
         return (
             <div className="ui segment">
                 <form action="" className="ui form">
                     <div className="field">
                         <label htmlFor="search">Image Search</label>
-                        <input type="text" onChange={this.onInputChange} name="search" id="search"/>
+                        <input type="text"
+                               onChange={this.onInputChange}
+                               name="search"
+                               id="search"
+                        />
                     </div>
                 </form>
             </div>
