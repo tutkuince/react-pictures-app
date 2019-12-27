3d4e228f14da3d79349d5bb677ce06f9386872f2
tutkuince <tutku.ince@outlook.com>
2019-12-27 16:33:43 +0300

    rendering list of components

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         });
 
         this.setState({images: response.data.results});
-    }
+    };
 
     render() {
         return (
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props);
-    return <div>ImageList</div>;
+    const images = props.images.map(image => {
+        return <img src={image.urls.regular} alt=""/>
+    });
+    return <div>{images}</div>
 };
 
 export default ImageList;
