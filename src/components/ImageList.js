7c3e841b11c71e93ca3bd81039cdcbd0cdf199ae
tutkuince <tutku.ince@outlook.com>
2019-12-27 22:06:06 +0300

    img alt to image.description

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageList = (props) => {
     const images = props.images.map(image => {
-        return <img key={image.id} src={image.urls.regular} alt=""/>
+        return <img key={image.id} src={image.urls.regular} alt={image.description}/>
     });
     return <div>{images}</div>
 };
