4fedf731f7e987e03d9f7f80912f89671e9b5f4c
tutkuince <tutku.ince@outlook.com>
2019-12-30 14:45:54 +0300

    callback on image load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,7 +8,11 @@ class ImageCard extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.imageRef);
+        this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
     }
 
     render() {
