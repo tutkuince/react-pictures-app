ada1d160ea2084aa5a6a3032a07d21999d480d14
tutkuince <tutku.ince@outlook.com>
2019-12-30 14:30:14 +0300

    creating an ImageCard component

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,11 +2,14 @@ import React from "react";
 
 class ImageCard extends React.Component {
     render() {
+
+        const {description, urls} = this.props.image;
+
         return (
             <div>
                 <img
-                    src={this.props.image.urls.regular}
-                    alt={this.props.image.description}
+                    src={urls.regular}
+                    alt={description}
                 />
             </div>
         );
